0ee379f6a805efe746ebca7e9980ec3a0427aae8
David Nunez <arizonatribe@gmail.com>
2017-10-13 11:11:32 -0700

    Missing a conditional render for the NavBarList component

--- src/components/layout/NavBarList.container.js
+++ src/components/layout/NavBarList.container.js
@@ -1,10 +1,12 @@
 import {path} from 'ramda'
 import {connect} from 'react-redux'
+import {renderConditional} from '../common/Conditional'
 
 import NavBarList from './NavBarList'
 
 const mapStateToProps = (store) => ({
-    items: path(['auth', 'menu', 'navigation'], store)
+    items: path(['auth', 'menu', 'navigation'], store),
+    condition: !!path(['auth', 'user', 'id'], store)
 })
 
-export default connect(mapStateToProps)(NavBarList)
+export default connect(mapStateToProps)(renderConditional(NavBarList))
