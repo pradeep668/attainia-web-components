bd493c3e5e2d404b9ad9b58db9ba79ce79bce06a
Ana Tomboulian <ana.tomboulian@attainia.com>
2017-10-16 10:01:02 -0700

    fix programming pattern for image

--- src/components/common/NotificationButton.js
+++ src/components/common/NotificationButton.js
@@ -6,7 +6,7 @@ import notificationicon from '../common/notificationicon.svg'
 const NotificationButton = props =>
     <Icon
         className="notification-icon"
-        imgSrc={(notificationicon && notificationicon.src) || notificationicon}
+        imgSrc={notificationicon}
         alt="N"
         {...props}
     />
