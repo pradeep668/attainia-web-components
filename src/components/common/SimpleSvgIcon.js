5475583db7f2da40aa07ed3e0901da9071a47859
David Nunez <arizonatribe@gmail.com>
2017-10-19 09:31:53 -0700

    Upgraded to v16

--- src/components/common/SimpleSvgIcon.js
+++ src/components/common/SimpleSvgIcon.js
@@ -1,33 +1,39 @@
 import {pickBy} from 'ramda'
-import {isNotNil} from 'ramda-adjunct'
+import {isNotEmpty, isNotNil} from 'ramda-adjunct'
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import uuid from 'uuid/v4'
 import {colors} from './constants'
+import {Conditional} from './Conditional'
 import icons from './icons.json'
 
 const Svg = styled.svg`display: block;`
 
 const SimpleSvgIcon = props => {
     const {icon, width, height, viewBox, fill, ...svgProps} = props
-    const {paths, transform, ...iconProps} = icons[icon]
+    const {paths, transform, ...iconProps} = icons[icon] || {paths: []}
 
     return (
-        <Svg
-            width={width || iconProps.width}
-            height={height || iconProps.height}
-            viewBox={viewBox || iconProps.viewBox || "0"}
-            {...svgProps}
-        >
-            <g
+        <Conditional condition={isNotNil(paths) && isNotEmpty(paths)}>
+            <Svg
+                width={width || iconProps.width}
+                height={height || iconProps.height}
                 {...pickBy(isNotNil, {
-                    transform,
-                    fill: props.fill || iconProps.fill
+                    viewBox: viewBox || iconProps.viewBox
                 })}
+                {...svgProps}
             >
-                {(paths || []).map(d => <path d={d} />)}
-            </g>
-        </Svg>
+                <g
+                    {...pickBy(isNotNil, {
+                        transform,
+                        fill: props.fill || iconProps.fill
+                    })}
+                >
+                    {paths.map(d => <path key={uuid()} d={d} />)}
+                </g>
+            </Svg>
+        </Conditional>
     )
 }
 
@@ -35,8 +41,8 @@ SimpleSvgIcon.propTypes = {
     fill: PropTypes.string,
     viewBox: PropTypes.string,
     icon: PropTypes.string.isRequired,
-    height: PropTypes.number,
-    width: PropTypes.number
+    height: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    width: PropTypes.oneOfType([PropTypes.number, PropTypes.string])
 }
 
 SimpleSvgIcon.defaultProps = {
