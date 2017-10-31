7a1549e5b7ace39a1869e5ccf0c08b1ce2e53f36
David Nunez <arizonatribe@gmail.com>
2017-10-30 18:21:44 -0700

    Minor changes to (temporarily) go for cjs bundling. Currently too many challenges getting minified es modules to work, and now running into an importing/exporting error with redux-auth-wrapper dependency

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,31 +1,14 @@
-import ContentCentered from './ContentCentered'
-import ContentFullSize from './ContentFullSize'
-import ContentHeader from './ContentHeader'
-import ContentWrapper from './ContentWrapper'
-import Footer from './Footer'
-import FooterContainer from './Footer.container'
-import Header from './Header'
-import HeaderContainer from './Header.container'
-import Layout from './Layout'
-import Main from './Main'
-import NavBarListContainer from './NavBarList.container'
-import NavBarList from './NavBarList'
-import NotFound from './NotFound'
-import Page from './Page'
-
-export {
-    ContentCentered,
-    ContentFullSize,
-    ContentHeader,
-    ContentWrapper,
-    Footer,
-    FooterContainer,
-    Header,
-    HeaderContainer,
-    Layout,
-    Main,
-    NavBarListContainer,
-    NavBarList,
-    NotFound,
-    Page
-}
+export {default as ContentCentered} from './ContentCentered'
+export {default as ContentFullSize} from './ContentFullSize'
+export {default as ContentHeader} from './ContentHeader'
+export {default as ContentWrapper} from './ContentWrapper'
+export {default as Footer} from './Footer'
+export {default as FooterContainer} from './Footer.container'
+export {default as Header} from './Header'
+export {default as HeaderContainer} from './Header.container'
+export {default as Layout} from './Layout'
+export {default as Main} from './Main'
+export {default as NavBarListContainer} from './NavBarList.container'
+export {default as NavBarList} from './NavBarList'
+export {default as NotFound} from './NotFound'
+export {default as Page} from './Page'
