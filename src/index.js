a3182d56d373b36530b38bc7b6ace8b04f7cd232
David Nunez <arizonatribe@gmail.com>
2017-11-06 14:08:52 -0700

    Adding Hot Module Reloading

--- src/index.js
+++ src/index.js
@@ -2,4 +2,16 @@ import {render} from 'react-dom'
 import Routes from './Routes'
 import './globals'
 
-render(Routes, document.querySelector('#root'))
+function renderApp(Component) {
+    render(Component, document.querySelector('#root'))
+}
+
+renderApp(Routes)
+
+if (module && module.hot) {
+    module.hot.accept('./Routes', () => {
+        /* eslint "global-require": "off" */
+        const DevRoutes = require('./Routes').default
+        renderApp(DevRoutes)
+    })
+}
