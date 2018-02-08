fb8ac44dcb14e7c383730747c8e4a476b8bc9eef
David Nunez <arizonatribe@gmail.com>
2018-02-08 11:21:49 -0700

    Fixed order of precedence for styles overrides in Button components. Also added a simple boolean flag for secondary color

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -4,8 +4,9 @@ import {getThemeProp, getProp} from './helpers'
 
 const Button = styled.button`
     background-color: ${props => (
-        getThemeProp(['colors', 'status', props.status])(props) ||
         getProp(['styles', 'backgroundColor'])(props) ||
+        (props.secondary && getThemeProp(['colors', 'secondary', 'default'])(props)) ||
+        getThemeProp(['colors', 'status', props.status])(props) ||
         getThemeProp(['colors', 'primary', 'default'], 'crimson')(props)
     )};
     color: ${props => (
@@ -18,8 +19,8 @@ const Button = styled.button`
     border-radius: 5px;
     font-family: ${getThemeProp(['fonts', 'fontFamily'], 'Arial')};
     font-size: ${props => (
-        getThemeProp(['fonts', 'fontSize'])(props) ||
-        getProp(['styles', 'fontSize'], '15px')(props)
+        getProp(['styles', 'fontSize'])(props) ||
+        getThemeProp(['fonts', 'fontSize'], '15px')(props)
     )};
     font-weight: 700;
     padding: ${getProp(['styles', 'padding'], '15px')};
@@ -31,6 +32,7 @@ const Button = styled.button`
 `
 Button.propTypes = {
     disabled: PropTypes.bool,
+    secondary: PropTypes.bool,
     styles: PropTypes.shape({
         fontSize: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
         color: PropTypes.string,
